b7d31e0a0f552bcd7735a0187d68e79822f17b56
safira.pusparanti <safira.pusparanti@gmail.com>
2017-10-08 13:23:02 +0700

    First commit

---
+++ out/artifacts/future_phase1_war_exploded/view/header.jsp
@@ -0,0 +1,30 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Safira
+  Date: 08-Oct-17
+  Time: 11:57 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@page import="java.util.Date"%>
+<nav class="blue-grey darken-4">
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo center">POS SYSTEM</a>
+        <ul class="left">
+            <li><a href="#">Role: username</a></li>
+            <li><a href="#">Date: <%=new Date()%></a></li>
+        </ul>
+        <ul class="right">
+            <li><a href="#">Logout</a></li>
+        </ul>
+    </div>
+</nav>
+<%--<!DOCTYPE html>--%>
+<%--<html>--%>
+<%--<head>--%>
+    <%--<title>Title</title>--%>
+<%--</head>--%>
+<%--<body>--%>
+
+<%--</body>--%>
+<%--</html>--%>
---
+++ src/Model/UserModel.java
@@ -0,0 +1,57 @@
+package Model;
+
+public class UserModel {
+    private String userId;
+    private String name;
+    private String password;
+    private String logtime;
+    private boolean status;
+
+    public UserModel(String userId, String name, String password, String logtime, boolean status) {
+        this.userId = userId;
+        this.name = name;
+        this.password = password;
+        this.logtime = logtime;
+        this.status = status;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogtime() {
+        return logtime;
+    }
+
+    public void setLogtime(String logtime) {
+        this.logtime = logtime;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
